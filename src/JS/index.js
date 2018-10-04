1f7255ca85fcfb239de3961755ecf3fb3161be7c
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-10-04 16:20:27 +0330

    AboutUs component added to index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -3,10 +3,12 @@ const ReactDOM = require('react-dom');
 
 const Nav = require('./components/Nav');
 const MainHeader = require('./components/MainHeader');
+const AboutUs = require('./components/AboutUs');
 
 const jsx = (<React.Fragment>
     <Nav />
 <MainHeader />
+<AboutUs />
 </React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
