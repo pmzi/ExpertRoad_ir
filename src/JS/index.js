2bac2beb18cf54a400f4ec8d770946e6dc3d196c
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-10-04 15:46:48 +0330

    mainHeader added to index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,7 +2,11 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 const Nav = require('./components/Nav');
+const MainHeader = require('./components/MainHeader');
 
-const jsx = (<Nav />);
+const jsx = (<React.Fragment>
+    <Nav />
+<MainHeader />
+</React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
