c576053415ef8bc1bcab6081cbf295ab2303b2ef
Pouya <pouya@Pouyas-MacBook-Pro.local>
2018-10-06 14:03:34 +0330

    feature component added to index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,11 +4,13 @@ const ReactDOM = require('react-dom');
 const Nav = require('./components/Nav');
 const MainHeader = require('./components/MainHeader');
 const AboutUs = require('./components/AboutUs');
+const Features = require('./components/Features');
 
 const jsx = (<React.Fragment>
     <Nav />
 <MainHeader />
 <AboutUs />
+<Features />
 </React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
