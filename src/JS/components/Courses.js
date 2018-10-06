1a2d9e5100ea58aaea73c527680f2ca6331a4474
Pouya <pouya@Pouyas-MacBook-Pro.local>
2018-10-06 18:50:59 +0330

    courses component has been designed

---
+++ src/JS/components/Courses.js
@@ -0,0 +1,28 @@
+const React = require('react');
+
+const CourseItem = require('./subComponents/CourseItem');
+
+class Courses extends React.Component{
+
+    render(){
+        return (
+            <section className='courses'>
+                <div className='courses__container layout'>
+                     <header className='courses__header'>
+                         <h4 className='courses__header-title'>
+                             دوره ها
+                         </h4>
+                     </header>
+                     <div className='courses__coursesWrapper'>
+                        <CourseItem image='course-1.jpg' title='HTML5 + CSS3' duration='56 saat' teacher='Pouya MozaffarMagham' link='' />
+                        <CourseItem image='course-1.jpg' title='' duration='' teacher='' link='' />
+                        <CourseItem image='course-1.jpg' title='' duration='' teacher='' link='' />
+                     </div>
+                </div>
+            </section>
+        );
+    }
+
+}
+
+module.exports = Courses;
