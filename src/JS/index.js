c0da5d5d352755956368e00b036154b6e624f209
Pouya <pouya@Pouyas-MacBook-Pro.local>
2018-10-06 18:51:34 +0330

    courses is now added to index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -5,12 +5,14 @@ const Nav = require('./components/Nav');
 const MainHeader = require('./components/MainHeader');
 const AboutUs = require('./components/AboutUs');
 const Features = require('./components/Features');
+const Courses = require('./components/Courses');
 
 const jsx = (<React.Fragment>
     <Nav />
 <MainHeader />
 <AboutUs />
 <Features />
+<Courses />
 </React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
