e15fc3fa56ed697ccbf95f08e03a8e99a7409c82
Pouya <pouya@Pouyas-MacBook-Pro.local>
2018-10-06 18:51:14 +0330

    courseItem subComponent has been designed

---
+++ src/JS/components/subComponents/CourseItem.js
@@ -0,0 +1,63 @@
+const React = require('react');
+
+const Button = require('../Button');
+
+class CourseItem extends React.Component{
+
+    render(){
+        return (
+            <div className='courseItem'>
+                <header className='courseItem__header'>
+                    <img className='courseItem__header-image' src={`dist/images/${this.props.image}`} />
+                </header>
+                <div className='courseItem__details'>
+                    <table className='courseItem__details-table'>
+                    <tbody>
+                        <tr>
+                            <td colSpan='2'>
+                                {this.props.title}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>
+                                    <i className='material-icons'> 
+                                        person
+                                    </i>
+                                    <span>
+                                        نام مدرس:
+                                    </span>
+                                </div>
+                            </td>
+                            <td>
+                                {this.props.teacher}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>
+                                    <i className='material-icons'> 
+                                        timer
+                                    </i>
+                                    <span>
+                                        مدت زمان:
+                                    </span>
+                                </div>
+                            </td>
+                            <td>
+                                {this.props.duration}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+                <footer className='courseItem__footer'>
+                <Button smallPaddingTop={true} className='courseItem__footer-button' href={this.props.link} text='اطلاعات بیشتر و ثبت نام' />
+                </footer>
+            </div>
+        );
+    }
+
+}
+
+module.exports = CourseItem;
