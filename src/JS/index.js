c45e7ef8d098a6e561d6f495ea7f522883a94abe
pmzi <pouya.mozafar@gmail.com>
2018-11-07 14:51:54 +0330

    showTeachers is now added to index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -7,14 +7,16 @@ const AboutUs = require('./components/AboutUs');
 const Features = require('./components/Features');
 const Courses = require('./components/Courses');
 const Departments = require('./components/Departments');
+const ShowTeachers = require('./components/ShowTeachers');
 
 const jsx = (<React.Fragment>
     <Nav />
-<MainHeader />
-<AboutUs />
-<Features />
-<Courses />
-<Departments />
+    <MainHeader />
+    <AboutUs />
+    <Features />
+    <Courses />
+    <Departments />
+    <ShowTeachers />
 </React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
