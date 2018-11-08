3d5b24c035a5dcd855bf9c880004f002f826d9c6
pmzi <pouya.mozafar@gmail.com>
2018-11-08 17:32:06 +0330

    testimonials is now rendered to the screen

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,6 +8,7 @@ const Features = require('./components/Features');
 const Courses = require('./components/Courses');
 const Departments = require('./components/Departments');
 const ShowTeachers = require('./components/ShowTeachers');
+const Testimonials = require('./components/Testimonials');
 
 const jsx = (<React.Fragment>
     <Nav />
@@ -17,6 +18,7 @@ const jsx = (<React.Fragment>
     <Courses />
     <Departments />
     <ShowTeachers />
+    <Testimonials />
 </React.Fragment>);
 
 ReactDOM.render(jsx, document.querySelector("#app"));
